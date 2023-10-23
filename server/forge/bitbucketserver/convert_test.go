fd585b29684a839ca7d6249b259dfa0e88926db5
Thor Anker Kvisgård Lange <tal@netic.dk>
2023-10-23 12:30:34 +0200

    fix: Do not trigger build on deleted refs

--- server/forge/bitbucketserver/convert_test.go
+++ server/forge/bitbucketserver/convert_test.go
@@ -111,6 +111,18 @@ func Test_helper(t *testing.T) {
 				},
 				{
 					from: &bb.RepositoryPushEvent{
+						Changes: []bb.RepositoryPushEventChange{
+							{
+								FromHash: "0000000000000000000000000000000000000000",
+								ToHash:   "1234567890abcdef",
+								Type:     bb.RepositoryPushEventChangeTypeDelete,
+							},
+						},
+					},
+					to: nil,
+				},
+				{
+					from: &bb.RepositoryPushEvent{
 						Event: bb.Event{
 							Date: bb.ISOTime(now),
 							Actor: bb.User{
